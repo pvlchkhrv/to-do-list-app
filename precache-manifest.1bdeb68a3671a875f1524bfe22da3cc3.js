56f22414e508e30ae840b9e79cffb47e44f55b7b
pvl <pvlchkhrv@gmail.com>
2021-09-15 17:55:44 +0300

    Updates

---
+++ precache-manifest.1bdeb68a3671a875f1524bfe22da3cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea331cdbc76dae3c353a1bd402d35f59",
+    "url": "/to-do-list-app/index.html"
+  },
+  {
+    "revision": "81f8e3f2777deb04c653",
+    "url": "/to-do-list-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "dabdf097f9d305bfda18",
+    "url": "/to-do-list-app/static/js/2.c159abd5.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/to-do-list-app/static/js/2.c159abd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f8e3f2777deb04c653",
+    "url": "/to-do-list-app/static/js/main.121c6f28.chunk.js"
+  },
+  {
+    "revision": "23205acc972f67c5871e",
+    "url": "/to-do-list-app/static/js/runtime-main.5c7cb262.js"
+  }
+]);
